b17d17a58efa306c9ec6521ef555018190d00f26
ruisjr2010 <ruisjr2005@gmail.com>
2018-10-17 12:39:47 -0300

    Correção do merge.

--- SysCar/database/dao.py
+++ SysCar/database/dao.py
@@ -5,6 +5,8 @@ __exe_name__ = 'SysCar'
 
 from database.factory import Factory
 
+__tablename__ = 'tabela'
+
 class Dao(object):
 
 	def __init__(self):
@@ -19,17 +21,48 @@ class Dao(object):
 		factory = Factory()
 		self.__connection = factory.get_connection(config.get_database())
 
-	def update(self, select_dict, where):
-		if select_dict is None:
-			raise Exception("Campos para UPDATE n�o foram informados.")
+    def insert(self, values_dict):
+        sql = 'INSERT INTO %s (%s) VALUES (%s)'
+        print(sql)
+	def update(self, campos_dict=None, where=None):
+        sql = 'UPDATE %s SET %s WHERE %s'
+        print(sql)
 
-		sql = " update %s set " % __table_name
+	
 
-	def insert(self, sql):
-		pass
+	def delete(self, where_dict=None):
+        sql = 'DELETE FROM %s %s'
+        where_str = ""
+        
+        if where_dict is not None:
+            where_str = " WHERE true "
+            for campo, valor in where_dict.items():
+                # TODO Tratar neste momento os tipos vindos do dicionário (Integer, String, Float, Date e Time)
+                where_str = where_str + " AND " + campo + '=' + str(self.__get_value_typed(valor))
+                print(where_str)
 
-	def delete(self, sql):
-		pass
+        sql = sql % (__tablename__, where_str)
+        print(sql)
 
-	def select(self, sql):
-		pass
+	def select(self, campos_dict=None, where_dict=None):
+        sql = 'SELECT %s FROM %s %s'
+        campos_str = ""
+        where_str = ""
+
+        if campos_dict is None:
+            campos_str = "*"
+
+        if where_dict is not None:
+            where_str = " WHERE true "
+            for campo, valor in where_dict.items():
+                # TODO Tratar neste momento os tipos vindos do dicionário (Integer, String, Float, Date e Time)
+                where_str = where_str + " AND " + campo + '=' + str(self.__get_value_typed(valor))
+                print(where_str)
+
+        sql = sql % (campos_str, __tablename__, where_str)
+        print(sql)
+
+   def __get_value_typed(self, value):
+        value_typed = ""
+        
+        return value_typed
